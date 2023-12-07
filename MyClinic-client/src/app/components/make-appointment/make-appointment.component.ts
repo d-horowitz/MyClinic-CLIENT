8d8e14c9406f9f204e5a4de364ef6ceb4656a17e
d-horowitz <102904217+d-horowitz@users.noreply.github.com>
2023-12-07 10:47:42 +0000

    2.4

--- MyClinic-client/src/app/components/make-appointment/make-appointment.component.ts
+++ MyClinic-client/src/app/components/make-appointment/make-appointment.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-make-appointment',
@@ -12,15 +13,23 @@ import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angul
 })
 export class MakeAppointmentComponent implements OnInit {
   specializations!: specialization[];
-  selectedSpec!: specialization;
+  doctors!: doctor[] | null;
+  patientId!: string | null;
+  schedule!: workday[];
 
-  specForm = new FormGroup({
-    specialization: new FormControl(),
+  appForm = new FormGroup({
+    specializationId: new FormControl(),
+    doctorId: new FormControl()
   })
 
 
-  constructor(private http: HttpClient) { };
+  constructor(private http: HttpClient, private route: ActivatedRoute) { };
   ngOnInit(): void {
+    this.route.parent?.paramMap.subscribe(
+      p => {
+        this.patientId = p.get("id");
+      }
+    );
     this.http.get<specialization[]>("https://localhost:7099/api/specializations")
       .subscribe(
         r => {
@@ -28,8 +37,40 @@ export class MakeAppointmentComponent implements OnInit {
         }
       )
   }
+  getDoctors() {
+    this.doctors = null;
+    this.appForm.controls.doctorId.setValue(null);
+    this.http.get<doctor[]>(`https://localhost:7099/api/specializations/${this.appForm.value.specializationId}/doctors`)
+      .subscribe(
+        r => {
+          this.doctors = r;
+        }
+      )
+  }
+  getSchedule() {
+    this.http.get<workday[]>(`https://localhost:7099/api/doctors/${this.appForm.value.doctorId}/schedule/${new Date().toJSON().split("T")[0]}`)
+    .subscribe(
+      r=>{
+        this.schedule = r;
+      }
+    )
+  }
 }
 type specialization = {
   id: number,
   name: string
+}
+type doctor = {
+  id: number,
+  name: string,
+  appointmentDuration: string,
+  specialization: specialization
+}
+type workday  = {
+  id:number,
+  date: string,
+  begin: string,
+  end: string,
+  appointments: any[],
+  doctorId: number
 }
