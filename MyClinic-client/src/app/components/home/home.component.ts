e791d09de2701c8f331449d9efad574233dbc80d
d-horowitz <102904217+d-horowitz@users.noreply.github.com>
2023-12-25 13:56:00 +0000

    2.6

--- MyClinic-client/src/app/components/home/home.component.ts
+++ MyClinic-client/src/app/components/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
+import { MatRippleModule } from '@angular/material/core';
 import { RouterModule } from '@angular/router';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatIconModule, RouterModule],
+  imports: [MatIconModule, RouterModule, MatRippleModule, MatButtonModule, MatTabsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
